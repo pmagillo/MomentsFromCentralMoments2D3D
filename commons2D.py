f41cd4f54d721cba6695b5e971701b75a7f83809
Paola Magillo <36791675+pmagillo@users.noreply.github.com>
2026-05-15 21:28:53 +0200

    Update commons2D.py

--- commons2D.py
+++ commons2D.py
@@ -2,24 +2,7 @@
 Common functions to all 2D methods.
 """
 
-def readPixels(file_name):
-  """
-  Read from file the coordinates of the full squares
-  and return them in list of ternes.
-  """
-  couples = []
-  f = open(file_name,"r")
-  for L in f:
-    coord = L.split()
-    if len(coord)==0: continue
-    x,y = [int(c) for c in coord]
-    couples.append((x,y))
-  f.close()
-  return couples
-
-
 def printMoments(MOME):
-  #print("Momenti su quadtree con algoritmo di Wu et al.")
   for p in range(4):
     for q in range(4):
         if p+q>3: continue
@@ -32,6 +15,7 @@ orders = [(0,0),(1,0),(0,1)]
 orders = orders + [(1,1),(2,0),(0,2)]
 orders = orders + [(3,0),(0,3),(2,1),(1,2)]
 
+from reading2D import adaptiveRead as readInput
 
 def main(arg, decomposF, preprocF, momentsF, title):
    """
@@ -47,8 +31,8 @@ def main(arg, decomposF, preprocF, momentsF, title):
 
    else:
         print("---Read pixels from file "+ arg[1])
-        input_pixels = readPixels(arg[1])
-        print("Number of black pixels:", len(input_pixels))
+        input_pixels = readInput(arg[1])
+        print("Number of black pixels:", input_pixels.number(1))
         elements = decomposF(input_pixels)
         print("Number of black elements:", elements.num_elem())
         if hasattr(elements,"max_pair"):
