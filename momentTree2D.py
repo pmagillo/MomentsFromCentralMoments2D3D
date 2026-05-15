2935e0b38ae13cf78193dd769d73b581caa576ad
Paola Magillo <36791675+pmagillo@users.noreply.github.com>
2026-05-15 21:56:57 +0200

    Update momentTree2D.py

--- momentTree2D.py
+++ momentTree2D.py
@@ -1,3 +1,7 @@
+"""
+Computation of moments of a 2D binary image by using a quadtree,
+according to the algorithm by Wu, Horng and Lee.
+"""
 from commons2D import orders
 from quadtree import QTR_Tree
 
@@ -16,17 +20,6 @@ def factorG(order, coord, side):
   coord3=coord2*coord
   return coord3*side +1.5*coord2*(side2-side) +coord*side3 -0.5*coord*(3*side2-side) +0.25*(side4-2*side3+side2)
 
-
-def prova(xx,yy,lato):
-  print("Monenti di ("+str(xx)+","+str(yy)+") lato "+str(lato))
-  #(0,0) lato 1 ok, lato 2 ok
-  #(1,2) lato 1 ok, lato 2 ok
-  for p in range(4):
-    for q in range(4):
-      if p+q>3: continue
-      mom = factorG(p, xx, lato)*factorG(q, yy, lato)
-      print("Momento ",p,q,"   = ", mom)
-  
 
 def quadtreeMoments(QT):
   """
@@ -54,5 +47,4 @@ from commons2D import main
 import sys
 
 if __name__ == "__main__":
-   #provaPrecalcoli()
    main(sys.argv, buildQuadtree, None, quadtreeMoments, "====2D Quadtree, old method.")
