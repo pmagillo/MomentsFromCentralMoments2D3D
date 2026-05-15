1904dabdb52a72c598e718e33bfb46cafd25afd8
Paola Magillo <36791675+pmagillo@users.noreply.github.com>
2026-05-15 22:12:48 +0200

    Update octree.py

--- octree.py
+++ octree.py
@@ -1,4 +1,3 @@
-
 """
 The octree encodes a 3D image of size LxLxL where
 L = 2^E for some natural E.
@@ -82,17 +81,14 @@ class OCT_Node:
     return 2**self.exponent
 
   def __str__(self):
-      S = str()
-      S = S+"Nodo con min "+str(self.xmin)+","+str(self.ymin)+","+str(self.zmin)
-      S = S+"; lato "+str(2**self.exponent)+"; colore "+str(self.color)
-      return S
+      return str(self.xmin)+' '+str(self.ymin)+' '+str(self.zmin)+' '+str(self.exponent)
  
 def power_of_two(L1, L2, L3):
   """
   Return the exponent E of the minimum power of two
   which is greater then or equal to all L1, L2 and L3.
   L1, L2, L3 must be positive integers (they are the
-  lengths of the three sides of the iumage).
+  lengths of the three sides of the image).
   """
   L = 1
   E = 0
@@ -155,10 +151,10 @@ class OCT_Tree:
     x,y,z = 0,0,0
     for e in loc_code:
       s = s//2
-      if e in (1,3,5,7): x += s
-      if e in (2,3,6,7): y += s
+      if e in (1,3,5,7): y += s
+      if e in (2,3,6,7): x += s
       if e in (4,5,6,7): z += s
-    return (x,y)
+    return (x,y,z)
   
   def code_for_pixel(self,x,y,z):
     """
@@ -191,35 +187,46 @@ class OCT_Tree:
     black_leaves = [C for C in self.leaves if self.leaves[C].color==1]
     return len(black_leaves)
 
-def stampa(Q):
-  print("Dimensioni ",Q.x_side, Q.y_side,Q.z_side)
-  print("Esponente ",Q.exponent)
+  def print_me(self, filename):
+    F = open(filename,'w')
+    F.write(str(self.x_side)+' '+str(self.y_side)+' '+str(self.z_side)+'\n')
+    F.write(str(self.exponent)+'\n')
+    F.write(str(self.side)+'\n')
+    for C in self.leaves:
+       if self.leaves[C].color==1:
+          s = ""
+          for digit in C: s = s+str(digit)
+          F.write(str(s)+'\n')
+          #F.write(str(s)+' '+str(self.leaves[C])+'\n')
+    F.close()
 
-  print("Foglie:")
-  for aa,bb in zip(Q.leaves.keys(),Q.leaves.values()): 
-      print("  ",aa," --> ",bb)   
+def readCode(thestring):
+  return tuple([int(c) for c in thestring])
 
-  NERI = [C for C in sorted(Q.leaves) if Q.leaves[C].color==1]
-  BIAN = [C for C in sorted(Q.leaves) if Q.leaves[C].color==0]
-  print("Foglie bianche")
-  for C in BIAN:
-    node = Q.leaves[C]
-    #print("Nodo ",C," min coord ",Q.min_pixel(C), " lato ",Q.node_side(C))
-    print("Nodo ",C," min coord ",node.xmin,node.ymin,node.zmin," lato ",node.side())
-  print("Foglie nere")
-  for C in NERI:
-    node = Q.leaves[C]
-    #print("Nodo ",C," min coord ",Q.min_pixel(C), " lato ",Q.node_side(C))
-    print("Nodo ",C," min coord ",node.xmin,node.ymin,node.zmin," lato ",node.side())
+def readOctree(filename):
+    F = open(filename,'r')
+    SX, SY, SZ = [int(p) for p in F.readline().split()]
+    newtree = OCT_Tree(SX, SY, SZ)
+    newtree.exponent = [int(p) for p in F.readline().split()][0]
+    newtree.side = [int(p) for p in F.readline().split()][0]
+    newtree.leaves = dict()
+    for L in F:
+      parts = L.split()
+      C = readCode(parts[0]) #location code
+      x0, y0, z0 = newtree.min_pixel(C)
+      sideC = newtree.node_side(C)
+      expon = newtree.exponent - len(C)
+      #x0, y0, z0, expon = [int(p) for p in parts[1:]]
+      newtree.leaves[C] = OCT_Node(x0,y0,z0, expon, 1)
+    F.close()
+    return newtree
 
-def octreeBuild(SX,SY,SZ, black_pixels=[]):
+
+def buildOctree(IMG):
   """
-  Build the octree for the given 3D image
-  which covers the cube [0,SX-1] x [0,SY-1] x [0,SZ-1]
-  and whose black pixels are contained in the list
-  black_pixels, the other pixels are white.
+  Build the octree for the given 3D image.
   """
-  Q = OCT_Tree(SX, SY, SZ)
+  Q = OCT_Tree(IMG.dimX, IMG.dimY, IMG.dimZ)
   Qside = 2**Q.exponent
   #print("Costruisco octree di lato ",Qside)
   #keep a queue of location codes that are candidate to
@@ -227,15 +234,14 @@ def octreeBuild(SX,SY,SZ, black_pixels=[]):
   candid = []
   #remove the root and add all individual black pixels as leaves
   del Q.leaves[()]
-  #print("Appena cancellata root, foglie=",Q.leaves)
-  for x,y,z in black_pixels:
-    C = Q.code_for_pixel(x,y,z)
-    Q.leaves[C] = OCT_Node(x,y,z)
-    Q.leaves[C].color = 1
-    if C[-1]==0: candid.append(parent(C))
-    #if (len(Q.leaves)%100)==0: print(" Ora ci sono ",len(Q.leaves)," nodi")
-
-  #print("Foglie messe (solo le nere), adesso provo a fondere")
+  for x in range(IMG.dimX):
+    for y in range(IMG.dimY):
+      for z in range(IMG.dimZ):
+        if IMG.get(x,y,z)==1:
+          C = Q.code_for_pixel(x,y,z)
+          Q.leaves[C] = OCT_Node(x,y,z)
+          Q.leaves[C].color = 1
+          if C[-1]==0: candid.append(parent(C))
   
   #scan the queue and try to merge nodes
   I = 0
@@ -243,124 +249,34 @@ def octreeBuild(SX,SY,SZ, black_pixels=[]):
     C = candid[I]
     I += 1
     present = [(C+(i,) in Q.leaves) for i in range(8)]
-    #qqq = input("Un tasto")
     if False in present: continue
-    #se ci sono tutti, sono tutti neri
+    #all present implies all black
     children = [Q.leaves[C+(i,)] for i in range(8)]
     #the eight children have equal color, so merge them
     for i in range(8): del Q.leaves[C+(i,)]
-    #determina punto di aggancio come baricentro
-    xx = children[0].xmin #0.25*sum([children.x for i in range(4)])
-    yy = children[0].ymin #0.25*sum([children.y for i in range(4)])
+    #anchor point 
+    xx = children[0].xmin
+    yy = children[0].ymin
     zz = children[0].zmin
     Q.leaves.update({C: OCT_Node(xx,yy,zz, children[0].exponent+1, 1 )})
-    #print("Appena aggiornato, foglie=")
-    #for aa,bb in zip(Q.leaves.keys(),Q.leaves.values()): print(aa,bb)   
     if C[-1]==0: candid.append(parent(C))
   return Q
 
-def Funziona_ma_lento_octreeBuild(SX,SY,SZ, black_pixels=[]):
-  """
-  Build the octree for the given 3D image
-  which covers the cube [0,SX-1] x [0,SY-1] x [0,SZ-1]
-  and whose black pixels are contained in the list
-  black_pixels, the other pixels are white.
-  """
-  Q = OCT_Tree(SX, SY, SZ)
-  Qside = 2**Q.exponent
-  #print("Costruisco octree di lato ",Qside)
-  #keep a queue of location codes that are candidate to
-  #be merged into a parent node
-  candid = []
-  #remove the root and add all individual pixels, white
-  del Q.leaves[()]
-  #print("Appena cancellata root, foglie=",Q.leaves)
-  for x in range(0,Qside):
-     for y in range(0,Qside): 
-       for z in range(0,Qside):
-          C = Q.code_for_pixel(x,y,z)
-          #print(" metto ",x,y, " codice ",C)
-          Q.leaves.update({C: OCT_Node(x,y,z)})
-          if (x,y,z) in black_pixels: Q.leaves[C].color = 1 #****************
-          if C[-1]==0: candid.append(parent(C))
-          #if (len(Q.leaves)%100)==0: print(" Ora ci sono ",len(Q.leaves)," nodi")
-  #print("Appena messi tutti bianchi, foglie=")
-  #for aa,bb in zip(Q.leaves.keys(),Q.leaves.values()): print(aa,bb)
-  #print(" e candidati ",candid)
-  #print("Num foglie:",  len(Q.leaves))
-  #print("Qtree edge:",Qside)
-  #assert len(Q.leaves)==(Qside*Qside)
-  #replace the color of all pixels in the list
-  """
-  **************** ALTERNATIVO A SOPRA
-  for x,y in black_pixels:
-    C = Q.code_for_pixel(x,y)
-    Q.leaves[C].color = 1
-  """
-  #print("Appena messi anche i neri, foglie=")
-  #for aa,bb in zip(Q.leaves.keys(),Q.leaves.values()): print(aa,bb)
-  #assert len(Q.leaves)==(Qside*Qside)
 
-  #print("Foglie messe, adesso provo a fondere")
-  
-  #scan the queue and try to merge nodes
-  I = 0
-  while I<len(candid):
-    C = candid[I]
-    I += 1
-    #print("Provo a fondere i figli di ",C) ###C tupla loc code
-    present = [(C+(i,) in Q.leaves) for i in range(8)]
-    #print("   Tutti presenti",present)
-    #qqq = input("Un tasto")
-    if False in present: continue
-    children = [Q.leaves[C+(i,)] for i in range(8)]
-    child_col = [children[i].color for i in range(8)]
-    if len(set(child_col))==1:
-      #print("   Allora fondo")
-      #the eight children have equal color, so merge them
-      for i in range(8): del Q.leaves[C+(i,)]
-      #determina punto di aggancio come baricentro
-      xx = children[0].xmin #0.25*sum([children.x for i in range(4)])
-      yy = children[0].ymin #0.25*sum([children.y for i in range(4)])
-      zz = children[0].zmin
-      Q.leaves.update({C: OCT_Node(xx,yy,zz, children[0].exponent+1,child_col[0])})
-      #print("Ho messo nodo con location code", C)
-      #print("Appena aggiornato, foglie=")
-      #for aa,bb in zip(Q.leaves.keys(),Q.leaves.values()): print(aa,bb)   
-      if C[-1]==0: candid.append(parent(C))
-  return Q
+import sys
+from reading3D import adaptiveRead as readInput
 
+def main(arg):
+    if len(arg)<=1:
+         print("Need 3D image name")
 
-def buildOctree(black_cubes):
-  """
-  Build and return the octree for the 3D image 
-  given as list of black cubes.
-  """
-  maxX = 1+max([c[0] for c in black_cubes])
-  maxY = 1+max([c[1] for c in black_cubes])
-  maxZ = 1+max([c[2] for c in black_cubes])
-  return octreeBuild(maxX, maxY, maxZ, black_cubes)
-
+    else:
+         print("---Read pixels from file "+ arg[1])
+         input_image = readInput(arg[1])
+         QT = buildOctree(input_image)
+         print("Number of black leaves: ",QT.num_elem())
+         QT.print_me("builtqtree_out.txt")
+         print("Leaves saved to builtqtree_out.txt")
 
 if __name__ == "__main__":
-  Q = OCT_Tree(6,6,4)
-  
-  """
-  print(Q.code_for_pixel(1,1,1))
-  print(Q.code_for_pixel(3,1,0))  
-  """
-  P = [(3,5,2),(2,5,2)]
-
-  for x in range(4): 
-    for y in range(4):
-      for z in range(4):
-         P.append((x,y,z))
-
-  for x in (4,5):
-    for y in (4,5):
-      P.append((x,y,2))
-      P.append((x,y,3))
-  print("Immagine, pixel neri ",P)
-  Q = octreeBuild(6,6,4, P)
-  
-  stampa(Q)
+   main(sys.argv)
